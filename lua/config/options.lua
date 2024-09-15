4b4b55bf411c6488105aef088b4889ef3cafe8ba
Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
2024-09-16 06:05:42 +0800

    options: add some options
    - Set relative-net mode: means the active window always Zoom Out
    - Set long line warp
    - Disable autoformat
    - Enable signcolumn all the time: https://stackoverflow.com/questions/78364323/im-having-trouble-with-flickering-in-lspsaga-nvim
    
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+-- Set relative-net mode: means the active window always Zoom Out
+opt.winheight = 99
+
+-- set long line warp
+opt.wrap = true
+
+-- disable autoformat
+vim.g.autoformat = false
+
+-- Enable signcolumn all the time: https://stackoverflow.com/questions/78364323/im-having-trouble-with-flickering-in-lspsaga-nvim
+opt.signcolumn = "yes"
