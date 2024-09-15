6a086ebedee0be329986f5deab334057b29e6c45
Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
2024-09-16 06:10:11 +0800

    keymaps: add some keymaps
    - Telesope grep with args
    - lspsaga
    
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap
+
+-- Has moved to plugins/telescope.lua
+-- keymap.set("n", "<leader>/", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
+
+-- lsp saga
+keymap.set("n", "gp", "<cmd>Lspsaga peek_definition<CR>", { silent = true, noremap = true })
+keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>")
+keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>") -- jump to previous diagnostic in buffer
+keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>")
