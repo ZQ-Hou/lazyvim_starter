881ad57e25069f3c35a317d1810a1d172d66bc1a
Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
2024-09-21 01:14:52 +0800

    options: disable expandtab
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- disable autoformat
+vim.g.autoformat = false
+
 local opt = vim.opt
 
 -- Set relative-net mode: means the active window always Zoom Out
@@ -10,8 +13,8 @@ opt.winheight = 99
 -- set long line warp
 opt.wrap = true
 
--- disable autoformat
-vim.g.autoformat = false
-
 -- Enable signcolumn all the time: https://stackoverflow.com/questions/78364323/im-having-trouble-with-flickering-in-lspsaga-nvim
 opt.signcolumn = "yes"
+
+-- Use tabs instead of translate to spaces
+opt.expandtab = false
